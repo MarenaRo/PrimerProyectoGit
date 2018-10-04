2815eb8c3941a12bc72390ed88b51a6e2d3416ce
Clases <Clases@A-006-4294>
2018-10-04 14:25:01 -0500

    Segundo commit

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,6 +4,9 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * Marena
+ */
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
